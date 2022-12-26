e2b8fe4169497122855e4423d53034cfa0156f1a
Muhammad Nadeem <112111506+Nadeem-innovent@users.noreply.github.com>
2022-12-26 23:03:10 +0400

    Add files via upload
    Navigation screen

--- App.js
+++ App.js
@@ -1,46 +1,97 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text,Button,TextInput, View } from 'react-native';
+import ClassComponent from './components/ClassComponent';
+import Login from './components/Login';
+import Functional from './components/Functional';
+import State from './components/State';
+import Textinput from './components/Textinput';
+import { Component } from 'react/cjs/react.production.min';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { color } from 'react-native-reanimated';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style= {styles.header}>
-      <Text>This is Nadeem from innovent</Text>
-      </View>
-      
-      <View style={styles.boldText}>
-      <Text style={styles.boldText}>This is Nadeem from innovent</Text>
-      </View>
 
-     {/* below code is inheritace of property. called (boldtext) property in body object view */}
-      <View style={styles.body}>
-      <Text style={styles.boldText}> This is Nadeem from innovent</Text>
-      <Text>This is Nadeem from innovent</Text>
+class App extends Component {
+
+  constructor (){
+
+    super();
+    this.State = {
+      Username : "",
+      Password : "",
+      Department : "",
+    }
+  }
+
+  submit(){
+    console.log("All values")
+    console.log(this.state)
+  }
+
+  render() {
+
+    return (
+      <View style={{flex : 1}}>
+         {/* <Login />
+        <ClassComponent />
+        <Functional />
+        <State />
+        <Textinput /> */}
+  
+  
+      <TextInput placeholder={"Enter Name"}
+      style = {styles.textBox}
+       onChangeText= {(text)=> {this.setState({Username:text})}}>
+      </TextInput>
+
+      <TextInput placeholder={"Enter Password"}
+      style = {styles.textBox}
+      secureTextEntry = {true}
+       onChangeText= {(text)=> {this.setState({Password:   text})}}>
+      </TextInput>
+
+      <TextInput placeholder={"Enter Department"}
+      style = {styles.textBox}
+       onChangeText= {(text)=> {this.setState({Department: text})}}>
+      </TextInput>
+  
+
+      {/* <Button title='Submit' onPress={()=>{this.submit()}}/> */}
+      <Button title='Go to profile' onPress={()=> {this.props.navigation.navigate('profile')}}/>
       </View>
-    </View>
-  );
+    );
+  } 
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles= StyleSheet.create({
+textBox : {
+borderColor : 'skyblue',
+borderWidth : 2,
+padding     : 10,
+marginHorizontal : 20,
+marginVertical : 25,
+}
+})
+
+
+function profile(props)
+ {
+   return(
+    <View style={{flex : 1, alignItems : 'center', justifyContent : 'center'}}>
+      <Text style={{fontSize : 50}}>
+        profile screen
+        </Text>
+        <Button title='Go to Home' onPress={()=> {props.navigation.navigate('Home')}}/>
+    </View>
+   ) 
+}
+const appNavigator = createStackNavigator ({
+  Home : {
+    screen : App
   },
 
-  header: {
-    backgroundColor : 'pink',
-    padding         : 20,
-  },
-
-  boldText: {
-    fontWeight: 'bold',
-    padding : 20,
-  },
-
-  body : {
-    backgroundColor: "green",
-    padding : 20,
+  profile: {
+    screen : profile
   }
-});
+})
+export default createAppContainer(appNavigator);
